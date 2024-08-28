8e579a44128c7d191c796f052c807c1b867fb30c
zjyezaire <zaire.elleby@gmail.com>
2024-08-28 11:58:22 -0400

    setup

---
+++ app.js
@@ -0,0 +1,20 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+  if (x >= y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
